92fd2b5d1a13b29992e2373b0ca928e00deb094c
Mark Ten <markten2002@gmail.com>
2025-03-11 20:05:16 +0300

    fix: fix bug with borrows orm model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,10 +1,12 @@
 from src.models.authors import AuthorsOrm
 from src.models.books import BooksOrm
 from src.models.users import UsersOrm
+from src.models.borrows import BorrowsOrm
 
 
 __all__ = [
     "AuthorsOrm",
     "BooksOrm",
     "UsersOrm",
+    "BorrowsOrm",
 ]
